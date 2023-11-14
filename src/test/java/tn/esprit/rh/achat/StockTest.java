a24c4991354292d4ff4afe973ba36c139d5c58c7
khaledgaaieb <khaled.gaaieb@esprit.tn>
2023-11-14 17:56:59 +0100

    updated nexus config

--- src/test/java/tn/esprit/rh/achat/StockTest.java
+++ src/test/java/tn/esprit/rh/achat/StockTest.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @ContextConfiguration(classes = {StockServiceImpl.class})
 @ExtendWith(SpringExtension.class)
-public class StockTest {
+class StockTest {
 
     @MockBean
     private StockRepository stockRepository;
